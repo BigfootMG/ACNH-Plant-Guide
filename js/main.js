83d8fe21bcf29fa42cf40c9e6a5f866d259653d2
Aeon <aeon_sake@hotmail.com>
2021-11-16 21:35:12 +0100

    fixed mobile menu not closing when clicking on a link

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 "use strict";
 
+var menu = document.querySelector('header .menu');
+
 //mobile menu toggle
-document.querySelector('header .menu .toggle').addEventListener('click', function() {
-    var menu = document.querySelector('header .menu');
+menu.querySelector('.toggle').addEventListener('click', function() {
     if (menu.classList.contains('open')) menu.classList.remove('open');
     else menu.classList.add('open');
 });
@@ -29,9 +30,11 @@ function closeDropdown() {
     }
 }
 
-//click outside of menu ot close dropdown
 document.addEventListener('click', function(e) {
-    var dropdown = document.querySelector('header .menu .dropdown.open');
-    if (dropdown === null) return;
-    if (dropdown !== e.target && !dropdown.contains(e.target)) closeDropdown();
+    //click on a link in the menu to close
+    if(e.target.tagName == 'A' && menu.contains(e.target)) menu.classList.remove('open');
+
+    //click outside of dropdown to close
+    var dropdown = document.querySelector('header .dropdown.open');
+    if (dropdown !== null && e.target !== dropdown && !dropdown.contains(e.target)) closeDropdown();
 });
